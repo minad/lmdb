eef1bd280e2d13728c5c851e93f6ddabafc965c7
Daniel Mendler <mail@daniel-mendler.de>
2013-09-08 07:15:53 +0200

    fix

--- ext/lmdb_ext/flag_parser.h
+++ ext/lmdb_ext/flag_parser.h
@@ -5,8 +5,10 @@ static int METHOD(VALUE key, VALUE value, int* flags) {
 #define FLAG(const, name) else if (id == rb_intern(#name)) { if (RTEST(value)) { *flags |= MDB_##const; } }
 #include FILE
 #undef FLAG
-        else
-                rb_raise(cError, "Invalid option %s", StringValueCStr(key));
+        else {
+                VALUE s = rb_inspect(key);
+                rb_raise(cError, "Invalid option %s", StringValueCStr(s));
+        }
 
         return 0;
 }
